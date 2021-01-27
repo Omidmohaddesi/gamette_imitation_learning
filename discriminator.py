update code for training condition 2 and fixed scaling actions, fixed zero division errors

discriminator.py
@@ -45,8 +45,9 @@ class Discriminator:
         loss, expert_score, stu_score, _ = self.sess.run(
             [self.loss, self.mean_expert_score, self.mean_stu_score, self.train_op], 
             feed_dict={
-                self.expert_state: np.stack([[np.array(list(d.values())).T for d in traj]
-                                             for traj in expert_state], axis=0),
+                # self.expert_state: np.stack([[np.array(list(d.values())).T for d in traj]
+                #                              for traj in expert_state], axis=0),
+                self.expert_state: expert_state,
                 self.expert_action: expert_action,
                 self.stu_state: stu_state,
                 self.stu_action: stu_action

expert/expert.py
@@ -15,6 +15,7 @@ from config import config
 import gym_crisp
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+import joblib
 
 
 crispPath = os.path.join(os.path.abspath('../..'), 'crisp/')
@@ -36,14 +37,14 @@ def scale_orders(my_df):
 def convert_actions(my_df):
 
     new_df = my_df.copy()
-    # new_df.loc[:, range(21, 56)] = pd.DataFrame(
-    #     np.where(new_df[range(21, 56)] == 'Prefer HC1', -1.,
-    #              np.where(new_df[range(21, 56)] == 'Prefer HC2', 0., 1.)),
-    #     columns=range(21, 56))
     new_df.loc[:, range(21, 56)] = pd.DataFrame(
-        np.where(new_df[range(21, 56)] == 'Prefer HC1', 0,
-                 np.where(new_df[range(21, 56)] == 'Prefer HC2', 1, 2)),
+        np.where(new_df[range(21, 56)] == 'Prefer HC1', -1.,
+                 np.where(new_df[range(21, 56)] == 'Prefer HC2', 0., 1.)),
         columns=range(21, 56))
+    # new_df.loc[:, range(21, 56)] = pd.DataFrame(
+    #     np.where(new_df[range(21, 56)] == 'Prefer HC1', 0,
+    #              np.where(new_df[range(21, 56)] == 'Prefer HC2', 1, 2)),
+    #     columns=range(21, 56))
 
     return new_df.astype(np.float32)
 
@@ -63,17 +64,24 @@ def main():
         ['player_id', 'condition'], axis=1)
 
     order_data.columns = order_data.columns.astype(int)
-    allocation_data.columns = allocation_data.columns.astype(int)
+    order_data_melt = order_data.melt(ignore_index=False)
+    order_data_melt['value'] = order_data_melt['value'].astype(int)
 
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    order_data_melt[['value']] = scaler.fit_transform(order_data_melt[['value']])
+
+    allocation_data.columns = allocation_data.columns.astype(int)
     allocation_data = convert_actions(allocation_data)
 
-    # scaled_order_data = scale_orders(order_data)
+    scaled_order_data = order_data_melt.pivot_table(
+        values='value', index=order_data_melt.index, columns='variable')
 
     if config.n_cpu == 1:
         env = FlattenObservation(
             FilterObservation(gym.make(config.env_name,
                                        study_name=config.study_name,
-                                       start_cycle=config.start_cycle),
+                                       start_cycle=config.start_cycle,
+                                       scaler=scaler),
                               filter_keys=config.obs_filter_keys))
         # env = DummyVecEnv([lambda: FilterObservation(gym.make(config.env_name,
         #                                                       study_name=config.study_name,
@@ -83,7 +91,8 @@ def main():
         env = SubprocVecEnv([lambda: FlattenObservation(
             FilterObservation(gym.make(config.env_name,
                                        study_name=config.study_name,
-                                       start_cycle=config.start_cycle),
+                                       start_cycle=config.start_cycle,
+                                       scaler=scaler),
                               filter_keys=config.obs_filter_keys))
                              for _ in range(config.n_cpu)])
 
@@ -94,8 +103,8 @@ def main():
     traj_state_np = np.array([])
     traj_action_np = np.array([])
 
-    # for orders, allocations in tqdm(zip(scaled_order_data.iterrows(), allocation_data.iterrows())):
-    for orders, allocations in tqdm(zip(order_data.iterrows(), allocation_data.iterrows())):
+    for orders, allocations in tqdm(zip(scaled_order_data.iterrows(), allocation_data.iterrows())):
+    # for orders, allocations in tqdm(zip(order_data.iterrows(), allocation_data.iterrows())):
 
         # prestop = False
         traj_states = []
@@ -136,6 +145,7 @@ def main():
 
     np.save('traj/{}_{}_state'.format(config.env_name, config.condition), traj_state_np)
     np.save('traj/{}_{}_action'.format(config.env_name, config.condition), traj_action_np)
+    joblib.dump(scaler, 'traj/{}_{}_scaler.joblib'.format(config.env_name, config.condition))
     print(traj_state_np.shape)
     print(traj_action_np.shape)
     print()

gym-crisp/gym_crisp/envs/crisp_env.py
@@ -17,11 +17,12 @@ logger = logging.getLogger(__name__)
 class CrispEnv2(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, n=35, role='ds', study_name='study_2_1', start_cycle=60):
+    def __init__(self, n=35, role='ds', study_name='study_2_1', start_cycle=60, scaler=None):
         self.n = n  # number of simulation periods in each episode
         self.np_random = None
         self.study_name = study_name
         self.start_cycle = start_cycle
+        self.order_action_scaler = scaler
         # self.data_columns = ['time', 'agent', 'item', 'value', 'unit']
         # self.data = pd.DataFrame(columns=self.data_columns)
         self.data = {
@@ -42,7 +43,7 @@ class CrispEnv2(gym.Env):
         self.total_reward = 0
         self.allocation_n = 3
         self.min_order = 0
-        self.max_order = 391
+        self.max_order = 400
         self.observation_keys = ['inventory', 'demand-hc1', 'demand-hc2', 'on-order', 'shipment',
                                  'suggestion', 'outl', 'dlv-rate-hc1', 'dlv-rate-hc2', 'mn-inventory', 'disruption']
         # self.action_space = spaces.Box(
@@ -178,8 +179,8 @@ class CrispEnv2(gym.Env):
         demand_hc2_c = sum(self.data.get('demand_hc2'))
         delivered_to_hc2_c = sum(self.data.get('delivered-to-hc2'))
 
-        deliv_rate_to_hc1 = min(round(delivered_to_hc1_c / demand_hc1_c, 2), 1)
-        deliv_rate_to_hc2 = min(round(delivered_to_hc2_c / demand_hc2_c, 2), 1)
+        deliv_rate_to_hc1 = min(round(delivered_to_hc1_c / demand_hc1_c if demand_hc1_c else 0, 2), 1)
+        deliv_rate_to_hc2 = min(round(delivered_to_hc1_c / demand_hc1_c if demand_hc1_c else 0, 2), 1)
 
         if self.simulation.disruptions and self.simulation.disruptions[0].happen_day_1 == self.simulation.now:
             disruption = 1
@@ -255,16 +256,17 @@ class CrispEnv2(gym.Env):
         rescaled_actions = np.zeros((2,), dtype=np.int64)
 
         bin_1 = np.arange(-1, 1, 2/self.allocation_n, dtype=np.float32)
-        bin_2 = np.arange(-1, 1, 2/self.max_order, dtype=np.float32)
+        # bin_2 = np.arange(-1, 1, 2/self.max_order, dtype=np.float32)
 
         rescaled_actions[0] = np.digitize(action[0], bin_1[1:])
-        rescaled_actions[1] = np.digitize(action[1], bin_2[1:])
+        # rescaled_actions[1] = np.digitize(action[1], bin_2[1:])
+        rescaled_actions[1] = self.order_action_scaler.inverse_transform([[action[1]]])[0][0]
 
         return rescaled_actions
 
     def _take_action(self, action):
 
-        if isinstance(action[0], np.float32) or isinstance(action[1], np.float32):
+        if isinstance(action[0], np.floating) or isinstance(action[1], np.floating):
             action = self._rescale_action(action)
 
         inventory = self.agent.inventory_level()

train.py
@@ -22,6 +22,8 @@ from buffer import Buffer
 import utils as U
 from pdb import set_trace as db
 import gym_crisp
+import joblib
+
 
 crispPath = os.path.join(os.path.abspath('../'), 'crisp/')
 crisp_server_Path = os.path.join(os.path.abspath('../'), 'crisp_game_server/')
@@ -37,13 +39,14 @@ np.set_printoptions(precision=4)
 # print(config)
 
 
-def get_env():
+def get_env(scaler):
     if config.n_cpu == 1:
         my_env = FlattenObservation(
             FilterObservation(
                 gym.make(config.env_name,
                          study_name=config.study_name,
-                         start_cycle=config.start_cycle),
+                         start_cycle=config.start_cycle,
+                         scaler=scaler),
                 filter_keys=config.obs_filter_keys))
         # my_env = DummyVecEnv([lambda: FilterObservation(gym.make(config.env_name,
         #                                                       study_name=config.study_name,
@@ -54,7 +57,8 @@ def get_env():
             FilterObservation(
                 gym.make(config.env_name,
                          study_name=config.study_name,
-                         start_cycle=config.start_cycle),
+                         start_cycle=config.start_cycle,
+                         scaler=scaler),
                 filter_keys=config.obs_filter_keys))
                                 for _ in range(config.n_cpu)])
     return my_env
@@ -64,13 +68,14 @@ def load_expert_traj():
 
     traj_np = np.load('expert/traj/{}_{}_state.npy'.format(config.env_name, config.condition), allow_pickle=True)
     theta_np = np.load('expert/traj/{}_{}_action.npy'.format(config.env_name, config.condition), allow_pickle=True)
+    scaler = joblib.load('expert/traj/{}_{}_scaler.joblib'.format(config.env_name, config.condition))
 
     print()
     print('expert trajectories shape: ', traj_np.shape)
     print('expert thetas shape: ', theta_np.shape)
     print()
     
-    return traj_np, theta_np
+    return traj_np, theta_np, scaler
 
 
 def code_reward_helper(stu_traj_code, r, code_1, code_2, code_3):
@@ -158,12 +163,12 @@ def gae(stu_traj_state, stu_traj_action, stu_traj_code):
 
 
 def main():
-    traj_np, theta_np = load_expert_traj()
+    traj_np, theta_np, scaler = load_expert_traj()
 
     global env, sess, policy, old_policy, value_net, old_value_net, \
         posterior, discriminator, buffer, saver, ckpt_name
 
-    env = get_env()
+    env = get_env(scaler)
     sess = U.get_tf_session()
     policy = Policy(config, sess, 'stu')
     old_policy = Policy(config, sess, 'old')